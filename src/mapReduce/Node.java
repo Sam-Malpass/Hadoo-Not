3742121dc9a39d3a8df8caeea9de7bb2d8109200
Sam <sam@malpass.me.uk>
2019-10-14 13:23:49 +0100

    Add variable to Node class

--- src/mapReduce/Node.java
+++ src/mapReduce/Node.java
@@ -6,9 +6,27 @@
  */
 package mapReduce;
 
+import java.util.ArrayList;
+
 public class Node implements Runnable {
+
+    /**
+     * thread holds a Thread object
+     */
+    private Thread thread;
+
+    /**
+     * threadID holds the ID of the thread
+     */
+    private String threadID;
+
+    /**
+     * task holds the Job to be run
+     */
+    private Job task;
+    
     @Override
     public void run() {
-        
+
     }
 }
