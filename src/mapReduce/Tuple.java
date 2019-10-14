431829304e9630e7233cd7630e9f1102289a99c9
Sam <sam@malpass.me.uk>
2019-10-14 12:45:07 +0100

    Add constructors to Tuple class

--- src/mapReduce/Tuple.java
+++ src/mapReduce/Tuple.java
@@ -15,4 +15,28 @@ public class Tuple {
      * value holds the value part of the tuple
      */
     private Object value;
+
+    /**
+     * Constructor with no arguments
+     * <p>
+     *     Sets up a null object and should not be used
+     * </p>
+     */
+    public Tuple() {
+        key = null;
+        value = null;
+    }
+
+    /**
+     * Constructor with arguments
+     * <p>
+     *     Sets up the Tuple object using the first argument as the key and the second argument as the value
+     * </p>
+     * @param k is the key to be used
+     * @param v is the value to be used
+     */
+    public Tuple(Object k, Object v) {
+        this.key = k;
+        this.value = v;
+    }
 }
