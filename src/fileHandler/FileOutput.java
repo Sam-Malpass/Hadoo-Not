01d971a2a67dba1a8b708e8edd5aacf9616ab0d0
Sam <sam@malpass.me.uk>
2019-10-14 13:07:41 +0100

    Add constructor to FileOutput class

--- src/fileHandler/FileOutput.java
+++ src/fileHandler/FileOutput.java
@@ -8,5 +8,13 @@ package fileHandler;
 
 public class FileOutput {
 
-    
+    /**
+     * Constructor with no arguments
+     * <p>
+     *     Sets up the object
+     * </p>
+     */
+    public FileOutput() {}
+
+
 }
