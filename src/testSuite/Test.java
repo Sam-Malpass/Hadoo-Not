7ac8b9476f67b091ce1744d9dae910d63287aa40
Sam <sam@malpass.me.uk>
2019-10-14 15:22:25 +0100

    Add fileReading function to Test class

--- src/testSuite/Test.java
+++ src/testSuite/Test.java
@@ -6,8 +6,18 @@
  */
 package testSuite;
 
+import fileHandler.FileHandler;
+
+import java.util.ArrayList;
+
 public class Test {
     public static void fileReading() {
-
+        FileHandler testHandler = new FileHandler();
+        ArrayList<String> list = testHandler.read("./src/AComp_Passenger_data.csv");
+        System.out.println("##### TESTING FILE READING #####");
+        for(String s : list) {
+            System.out.println(s);
+        }
+        System.out.println("##### FILE READING TEST COMPLETE #####");
     }
 }
