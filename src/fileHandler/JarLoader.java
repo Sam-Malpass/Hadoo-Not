561d8f1d714510b0bf1848a23215d3136088a70f
Sam <sam@malpass.me.uk>
2019-10-14 18:02:44 +0100

    Add getClassNames function to JarLoader

--- src/fileHandler/JarLoader.java
+++ src/fileHandler/JarLoader.java
@@ -6,6 +6,11 @@
  */
 package fileHandler;
 
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+
 public class JarLoader {
 
     /**
@@ -15,4 +20,35 @@ public class JarLoader {
      * </p>
      */
     public JarLoader() {}
+
+    /**
+     * Function getClassNames()
+     * <p>
+     *     Load and scan a JAR file for all the classes inside
+     * </p>
+     * @param jarName is the JAR file to scan
+     * @return the list of classes
+     */
+    public ArrayList<String> getClassNames(String jarName) {
+        ArrayList<String> classes = new ArrayList<>();
+        try {
+            JarInputStream jarFile = new JarInputStream(new FileInputStream(jarName));
+            JarEntry jarEntry;
+            while(true) {
+                jarEntry = jarFile.getNextJarEntry();
+                if(jarEntry == null) {
+                    break;
+                }
+                if(jarEntry.getName().endsWith(".class")) {
+                    classes.add(jarEntry.getName().replaceAll("/", "\\."));
+                }
+            }
+        }
+        catch (Exception e) {
+            System.err.println("[ERROR] Failed to load JAR");
+        }
+        return classes;
+    }
+
+    public
 }
