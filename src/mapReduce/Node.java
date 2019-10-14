1dc24795faf0261d217278c7a1f954e70c4cf5c8
Sam <sam@malpass.me.uk>
2019-10-14 13:21:30 +0100

    Create Node class

---
+++ src/mapReduce/Node.java
@@ -0,0 +1,14 @@
+/**
+ * Node
+ * @author Sam Malpass
+ * @version 0.0.0
+ * @since 0.0.0
+ */
+package mapReduce;
+
+public class Node implements Runnable {
+    @Override
+    public void run() {
+        
+    }
+}
