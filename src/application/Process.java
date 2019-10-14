836ffebcc991673db154e42f6c2ebad9c40cd266
Sam <sam@malpass.me.uk>
2019-10-14 14:01:41 +0100

    Add variables to Process class

--- src/application/Process.java
+++ src/application/Process.java
@@ -6,5 +6,39 @@
  */
 package application;
 
+import mapReduce.Node;
+import mapReduce.Tuple;
+import java.util.ArrayList;
+
 public class Process {
+
+    /**
+     * blockSize holds the number of data entries to be sent to a single mapper node
+     */
+    private int blockSize;
+
+    /**
+     * jobName is the name of the class file to use as the Job
+     */
+    private String jobName;
+
+    /**
+     * mapperNodes holds a list of all the Nodes that map
+     */
+    private ArrayList<Node> mapperNodes = new ArrayList<>();
+
+    /**
+     * reducerNodes holds a list of all the Nodes that reduce
+     */
+    private ArrayList<Node> reducerNodes = new ArrayList<>();
+
+    /**
+     * shuffledOutput holds a sorted list of all map Node outputs
+     */
+    private ArrayList<Tuple> shuffledOutput = new ArrayList<>();
+
+    /**
+     * finalOutput holds the results from the reduce Nodes
+     */
+    private ArrayList<Tuple> finalOutput = new ArrayList<>();
 }
