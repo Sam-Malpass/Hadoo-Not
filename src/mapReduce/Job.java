14eed6ae0c4ea91ef3c0caa35c90f855ed76905d
Sam <sam@malpass.me.uk>
2019-10-14 12:40:11 +0100

    Declare preprocess function

--- src/mapReduce/Job.java
+++ src/mapReduce/Job.java
@@ -6,5 +6,24 @@
  */
 package mapReduce;
 
+import java.util.ArrayList;
+
 public abstract class Job {
+    /**
+     * Constructor with no arguments
+     * <p>
+     *     Sets up the object
+     * </p>
+     */
+    public Job() {}
+
+    /**
+     * Function preprocess()
+     * <p>
+     *     User can define data conversion and cleaning in this step
+     * </p>
+     * @param input is the input data in the form of a list of lines, each of which represents a data entry
+     * @return a list of Objects that accurately represent the data in the system
+     */
+    public abstract ArrayList<Object> preprocess(ArrayList<String> input);
 }
