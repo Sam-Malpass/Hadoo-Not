5417e14f3554b27336fe2145f1ac951eba50ca30
Sam <sam@malpass.me.uk>
2019-10-14 12:43:09 +0100

    Add variables to Tuple class

--- src/mapReduce/Tuple.java
+++ src/mapReduce/Tuple.java
@@ -1,4 +1,18 @@
+/**
+ * Tuple
+ * @author Sam Malpass
+ * @version 0.0.0
+ * @since 0.0.0
+ */
 package mapReduce;
 
 public class Tuple {
+    /**
+     * key holds the key part of the Tuple
+     */
+    private Object key;
+    /**
+     * value holds the value part of the tuple
+     */
+    private Object value;
 }
