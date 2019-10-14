c5cd3fef990e3f1b0e3f02fbd43305a47444b303
Sam <sam@malpass.me.uk>
2019-10-14 13:04:44 +0100

    Add readFile function to FileInput class

--- src/fileHandler/FileInput.java
+++ src/fileHandler/FileInput.java
@@ -6,6 +6,10 @@
  */
 package fileHandler;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class FileInput {
 
     /**
@@ -15,4 +19,27 @@ public class FileInput {
      * </p>
      */
     public FileInput() {}
+
+    /**
+     * Function readFile()
+     * <p>
+     *     Reads the lines from a file to an ArrayList, then returns said ArrayList
+     * </p>
+     * @param filePath is the file to read from
+     * @return the list of lines
+     */
+    public ArrayList<String> readFile(String filePath) {
+        ArrayList<String> lines = new ArrayList<>();
+        String line;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            while((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        }
+        catch (Exception e) {
+            System.err.println("[ERROR] File not found");
+        }
+        return lines;
+    }
 }
