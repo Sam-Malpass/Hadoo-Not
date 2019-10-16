00065a242577c2d19fe9c638b3ad19743c78bea1
Sam <sam@malpass.me.uk>
2019-10-16 14:55:08 +0100

    Add variables to GUI class

--- src/graphicalUserInterface/GUI.java
+++ src/graphicalUserInterface/GUI.java
@@ -6,5 +6,28 @@
  */
 package graphicalUserInterface;
 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class GUI  {
+
+    /**
+     * windowHeight holds the height of the window
+     */
+    private double windowHeight = 720;
+
+    /**
+     * windowWidth holds the width of the window
+     */
+    private double windowWidth = 1280;
+
+    /**
+     * mainStage holds the Stage for the application
+     */
+    private Stage mainStage;
+
+    /**
+     * mainScene holds the Scene for the application
+     */
+    private Scene mainScene;
 }
