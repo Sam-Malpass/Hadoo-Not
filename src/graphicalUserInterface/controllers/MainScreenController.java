c38d84ac828e0d9735773d5bffcd6b37a114e7f6
Sam <sam@malpass.me.uk>
2019-10-16 15:49:42 +0100

    Start linking FXML to Controller

--- src/graphicalUserInterface/controllers/MainScreenController.java
+++ src/graphicalUserInterface/controllers/MainScreenController.java
@@ -6,13 +6,56 @@
  */
 package graphicalUserInterface.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainScreenController implements Initializable {
+    @FXML
+    private TextArea console;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        console.setEditable(false);
+    }
 
+    @FXML
+    private void setupJob() {
+
+    }
+
+    @FXML
+    private void run() {
+
+    }
+
+    @FXML
+    private void about() {
+
+    }
+
+    @FXML
+    private void help() {
+
+    }
+
+    @FXML
+    private void openOutput() {
+
+    }
+
+    public TextArea getConsole() {
+        return console;
+    }
+
+    public void writeConsole(String text) {
+        console.appendText(text);
+    }
+
+    public void clearConsole() {
+        console.clear();
     }
 }
