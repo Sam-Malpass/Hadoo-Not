908a4565ebbe43f238f863b58e311061297a9221
Unknown <sam@malpass.me.uk>
2019-10-17 15:19:34 +0100

    Create MapNode class

---
+++ src/application/nodes/MapNode.java
@@ -0,0 +1,27 @@
+/**
+ * MapNode
+ * @author Sam Malpass
+ * @version 0.0.4
+ * @since 0.0.4
+ */
+package application.nodes;
+
+public class MapNode extends Node {
+
+
+    /**
+     * Constructor with arguments
+     * <p>
+     * Takes the mode and the ID and prepares the Node for execution of a Job part
+     * </p>
+     * @param id is the ID for the node
+     */
+    public MapNode(String id) {
+        super(id);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
