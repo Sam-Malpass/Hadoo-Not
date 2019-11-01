0f3ca0d0f41e62df1167971d4e5bfec3fbaa2fb7
Unknown <sam@malpass.me.uk>
2019-11-01 16:45:43 +0000

    Write constructor for Chain class

--- src/application/Chain.java
+++ src/application/Chain.java
@@ -6,5 +6,27 @@
  */
 package application;
 
+import java.util.ArrayList;
+
 public class Chain {
+
+    /**
+     * jobChain holds a list of all Jobs to be performed in the process
+     */
+    ArrayList<Process> jobChain;
+
+    /**
+     * Constructor with arguments
+     * <p>
+     *
+     * </p>
+     * @param jarPath is the JAR to load jobs from
+     * @param jobNames the list of jobs to be performed in order
+     */
+    public Chain(String jarPath, ArrayList<String> jobNames) {
+        for(String s : jobNames) {
+            Process p = new Process(jarPath, s);
+            jobChain.add(p);
+        }
+    }
 }
